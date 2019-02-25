c56e0bab0363c49a7d71ce1488efcf93a9e6a5fd
Ashishpf <ashish.sondagar@pepperfry.com>
2019-02-25 19:06:44 +0530

    ReadFiles </> Mon, 2019-02-25, 19:06:44 IST 056

---
+++ Code/ReadFiles/main.go
@@ -0,0 +1,26 @@
+package main
+
+
+import (
+	"fmt"
+	)
+
+
+func main() {
+    fmt.Println("Hello Go Developer.. :]")
+
+
+    fmt.Println("\n") // The End.. .
+}
+
+
+/* Output:
+ashishs@lp-0731:~/Downloads/storage/Go/GoLang/Code$ go run Demo.go 
+Hello Go Developer.. :]
+*/
+
+
+/* Title
+-> Description..
+*/
+n.
